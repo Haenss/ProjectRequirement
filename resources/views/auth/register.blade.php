94a0d347ffa13e2e44bcd3b74436af1a13c6aadd
Ahmes <110957328+Haenss@users.noreply.github.com>
2022-11-07 15:21:40 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,15 +11,11 @@
                             @csrf
                             
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="id"  id="id" id="id" value="{{ old('id') }}" class="form-control" required autofocus>
+                                <input type="text" placeholder="Nama"  name="name" id="name" value="{{ old('name') }}" class="form-control" required autofocus>
                             </div>
 
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Nama"  name="nama" id="nama" value="{{ old('nama') }}" class="form-control" required autofocus>
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="BirthYear" name="date" id="date" value="{{ old ('date') }}" class="form-control" required
+                                <input type="text" placeholder="BirthDay" name="date" id="date" value="{{ old ('date') }}" class="form-control" required
                                     autofocus>
                                 @if ($errors->has('date'))
                                 <span class="text-danger">{{ $errors->first('date') }}</span>
@@ -37,55 +33,42 @@
                             <div class="form-group mb-3">
                                 <input type="number" placeholder="Phone" name="phone" id="phone" value="{{ old('phone') }}" class="form-control" required>
                                 @if ($errors->has('phone'))
-                                <span class="text-danger">{{ $errors->first('phone') }}</span>
+                                <span class="text-danger">{{ $errors->first('phone' ) }}</span>
                                 @endif
                                 <div class="form-group mb-3">
                             </div>
                         <div class="form-group mb-3">
-                             <select name="jobs" id="jobs"  class="form-control"  required
+                             <select name="job" id="job" class="form-control"  required
                                 autofocus>
-                                <option value="" disabled selected hidden> Jobs</option>
-                                <option value="BackEnd Engineer">BackEnd Engineer</option>
-                                <option value="FrontEnd Engineer">FrontEnd Engineer</option>
-                                <option value="DevOps Engineer">DevOps Engineer</option>
-                                <option value="UI/UX Engineer">UI/UX Engineer</option>
-                                <option value="Network Engineer">Network Engineer</option>
-                                <option value="Mobile Engineer">Mobile Engineer</option>
-                                <option value="Software Quality Engineer">Software Quality Engineer</option>
+                                <option value="" disabled selected hidden> Job</option>
+                                <option value="FrontEnd">FrontEnd</option>
+                                <option value="BackEnd">BackEnd</option>
+                                <option value="UI/UX">UI/UX</option>
+                                <option value="FullStack">FullStack</option>
+                                <option value="Mobile">Mobile</option>
                             </select> 
-                            @if ($errors->has('jobs'))
-                             <span class="text-danger">{{ $errors->first('jobs') }}</span>
+                            @if ($errors->has('job'))
+                             <span class="text-danger">{{ $errors->first('job') }}</span>
                             @endif
                         </div>
                             
                         <div class="form-group mb-3">
-                            <select  name="skill" id="skills"  class="form-control" required
+                            <select  name="skill" id="skill" class="form-control" required
                             autofocus>
-                                <option value="" disabled selected hidden>Skills</option>
+                                <option value="" disabled selected hidden>Skill_Set</option>
+                                <option value="Vue JS">Vue JS</option>
                                 <option value="Laravel">Laravel</option>
-                                <option value="Codeigniter">Codeigniter</option>
                                 <option value="Figma">Figma</option>
-                                <option value="API">API</option>
-                                <option value="Postgresql">Postgresql</option>
-                                <option value="Mysql">Mysql</option>
-                                <option value="Docker">Docker</option>
-                                <option value="Linux">Linux</option>
-                                <option value="ReactJS">ReactJS</option>
-                                <option value="VueJS">VueJS</option>
-                                <option value="Java">Java</option>
-                                <option value="Kotlin">Kotlin</option>
                                 <option value="Flutter">Flutter</option>
-                                <option value="PHP">PHP</option>
-                                <option value="JavaScript">JavaScript</option>
                             </select> 
                              @if ($errors->has('skill'))
-                                <span class="text-danger">{{ $errors->first('skills') }}</span>
+                                <span class="text-danger">{{ $errors->first('skill') }}</span>
                             @endif
                         </div>
 
                     </div>
                             <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-register btn-danger btn-block">Uploads</button>
+                                <button type="submit" class="btn btn-uploads btn-danger btn-block">Upload</button>
                             </div>
                         </form>
                     </div>
@@ -95,7 +78,6 @@
     </div>
 </main>
 @endsection
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
@@ -103,65 +85,15 @@
 <script>
     $(document).ready(function() {
 
-        $(".btn-register").click( function() {
+        $(".btn-uploads").click( function() {
 
-            var nama = $("#nama").val();
+            var name = $("#name").val();
             var date = $("#date").val();
             var email = $("#email").val();
             var phone = $("#phone").val();
-            var jobs = $("#jobs").val();
-            var skills = $("#skills").val();
-            var token = $("meta[name='csrf-token']").attr("content");
-
-            if (nama.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'Nama Wajib Diisi !'
-                });
-
-            }else if(date.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'UlangTahun Wajib Diisi !'
-                });
-
-            } else if(email.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'Alamat Email Wajib Diisi !'
-                });
-
-            } else if(phone.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'Phone Wajib Diisi !'
-                });
-
-            }else if(job.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'jobs Wajib Diisi !'
-                });
-
-            }else if(skill.length == "") {
-
-                Swal.fire({
-                    type: 'warning',
-                    title: 'Oops...',
-                    text: 'skills Wajib Diisi !'
-                });
-
-            }else {
+            var job = $("#job").val();
+            var skill = $("#skill").val();
+            var token = $("meta[name='csrf-token']").attr("content"); {
 
                 //ajax
                 $.ajax({
@@ -170,12 +102,12 @@
                     type: "POST",
                     cache: false,
                     data: {
-                        "nama"  : nama,
-                        "date"  : date,
-                        "email" : email,
-                        "phone" : phone,
-                        "jobs"   : jobs,
-                        "skills" : skills,
+                        "name": name,
+                        "date": date,
+                        "email": email,
+                        "phone": phone,
+                        "job": job,
+                        "skill": skill,
                         "_token": token
                     },
 
@@ -189,12 +121,12 @@
                                 text: 'silahkan login!'
                             });
 
-                            $("#nama").val('');
+                            $("#name").val('');
                             $("#date").val('');
                             $("#email").val('');
                             $("#phone").val('');
-                            $("#jobs").val('');
-                            $("#skills").val('');
+                            $("#job").val('');
+                            $("#skill").val('');
 
                         } else {
 
@@ -209,15 +141,6 @@
                         console.log(response);
 
                     },
-
-                    error:function(response){
-                        Swal.fire({
-                            type: 'error',
-                            title: 'Opps!',
-                            text: 'server error!'
-                        });
-                    }
-
                 })
 
             }
@@ -226,5 +149,4 @@
 
     });
 </script>
-
 
